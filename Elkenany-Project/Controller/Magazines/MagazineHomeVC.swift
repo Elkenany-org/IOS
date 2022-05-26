04d4cc9cb8852a9e33ecda86c047a2438ed4c25a
AbdelazizReda <48037088+AbdelazizReda@users.noreply.github.com>
2022-05-26 14:53:25 +0200

    filters

--- Elkenany-Project/Controller/Magazines/MagazineHomeVC.swift
+++ Elkenany-Project/Controller/Magazines/MagazineHomeVC.swift
@@ -9,7 +9,7 @@ import UIKit
 import JGProgressHUD
 
 class MagazineHomeVC: UIViewController {
-
+    
     //MARK:Outlets
     @IBOutlet weak var sectorsCV: UICollectionView!
     @IBOutlet weak var searchView: UIView!
@@ -45,7 +45,7 @@ class MagazineHomeVC: UIViewController {
         magazinTV.delegate = self
         sectorsCV.delegate = self
         sectorsCV.dataSource = self
-
+        
         self.sectorsCV.register(UINib(nibName: "SelectedSectorCell", bundle: nil), forCellWithReuseIdentifier: "SelectedSectorCell")
         magazinTV.register(UINib(nibName: "companiesCell", bundle: nil), forCellReuseIdentifier: "companiesCell")
         magazinTV.prefetchDataSource = self
@@ -53,10 +53,10 @@ class MagazineHomeVC: UIViewController {
         magazinTV.estimatedRowHeight = 150
         magazinTV.rowHeight = UITableView.automaticDimension
     }
-
-
+    
+    
     func FeatchDataOfectores(){
-      
+        
         DispatchQueue.global(qos: .background).async {
             let param = ["type": "farm"  , "page": "\(self.currentpaga)"]
             let companyGuide = "https://elkenany.com/api/magazine/magazines?type=&sort="
@@ -85,7 +85,7 @@ class MagazineHomeVC: UIViewController {
                     print("============ error \(error)")
                 }
                 //Data Wrong From Server
-
+                
                 else if let loginError = filier {
                     print("--========== \(loginError.error?.localizedCapitalized ?? "") ")
                 }
@@ -93,7 +93,7 @@ class MagazineHomeVC: UIViewController {
                 else {
                     if success?.data?.nextPageURL == nil {
                     }
-                
+                    
                     let successDataa = success?.data?.data ?? []
                     print("current", self.currentpaga)
                     self.magazinSubModel.append(contentsOf: successDataa)
@@ -108,8 +108,8 @@ class MagazineHomeVC: UIViewController {
     }
     
     
-
-
+    
+    
     func SearchService(){
         //Handeling Loading view progress
         let hud = JGProgressHUD(style: .dark)
@@ -149,62 +149,55 @@ class MagazineHomeVC: UIViewController {
         searchView.isHidden = false
         view1.isHidden = true
         view2.isHidden = true }
-
-
-}
-
     
-    //MARK:- searchBAr delegets
-    extension MagazineHomeVC : UISearchBarDelegate {
-        func setupSearchBar() {
-            searchBar.delegate = self
+    }
+
+
+//MARK:- searchBAr delegets
+extension MagazineHomeVC : UISearchBarDelegate {
+    func setupSearchBar() {
+        searchBar.delegate = self
+    }
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchText.isEmpty == false {
+            //your model
+            let data = MagazineModel?.data?.data ?? []
+            //your array
+            magazinSubModel = data.filter({ ($0.name?.contains(searchText) ?? (0 != 0)) })
+            magazinSubModel.removeAll()
+            //Api func
+            SearchService()
         }
-        func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-            if searchText.isEmpty == false {
-                //your model
-                let data = MagazineModel?.data?.data ?? []
-                //your array
-                magazinSubModel = data.filter({ ($0.name?.contains(searchText) ?? (0 != 0)) })
-                magazinSubModel.removeAll()
-                //Api func
-                SearchService()
-            }
-            //reload
-            magazinTV.reloadData()
-        }
-
-
-        func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
-            if let cBtn = searchBar.value(forKey: "cancelButton") as? UIButton {
-                cBtn.setTitle("الغاء", for: .normal)
-                searchBar.tintColor = #colorLiteral(red: 0.189121604, green: 0.4279403687, blue: 0.1901243627, alpha: 1)
-                searchBar.text = ""
-            }}
-
-
-        func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
-            searchView.isHidden = true
+        //reload
+        magazinTV.reloadData()
+    }
+    
+    
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        if let cBtn = searchBar.value(forKey: "cancelButton") as? UIButton {
+            cBtn.setTitle("الغاء", for: .normal)
+            searchBar.tintColor = #colorLiteral(red: 0.189121604, green: 0.4279403687, blue: 0.1901243627, alpha: 1)
             searchBar.text = ""
-            view1.isHidden = false
-            view2.isHidden = false
-//            magazinSubModel.removeAll()
-            FatchDatafromHome()
-            let hud = JGProgressHUD(style: .dark)
-            hud.textLabel.text = "جاري التحميل"
-            hud.show(in: self.view)
-            hud.dismiss()
         }}
-
-
     
     
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchView.isHidden = true
+        searchBar.text = ""
+        view1.isHidden = false
+        view2.isHidden = false
+        //            magazinSubModel.removeAll()
+        FatchDatafromHome()
+        let hud = JGProgressHUD(style: .dark)
+        hud.textLabel.text = "جاري التحميل"
+        hud.show(in: self.view)
+        hud.dismiss()
+    }}
 
 
 
 
-
-
-//pagination extension
+//MARK:pagination extension
 extension MagazineHomeVC:UITableViewDataSourcePrefetching {
     func tableView(_ tableView: UITableView, prefetchRowsAt indexPaths: [IndexPath]) {
         for index in indexPaths {
@@ -218,13 +211,14 @@ extension MagazineHomeVC:UITableViewDataSourcePrefetching {
 
 
 
+
 //MARK:- TableView for companies  [Methods + Delegets]
 extension MagazineHomeVC:UITableViewDelegate,UITableViewDataSource{
-
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return magazinSubModel.count
     }
-
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if let Companiescell = tableView.dequeueReusableCell(withIdentifier: "companiesCell") as? companiesCell{
             Companiescell.companyName.text = magazinSubModel[indexPath.row].name ?? ""
@@ -234,16 +228,13 @@ extension MagazineHomeVC:UITableViewDelegate,UITableViewDataSource{
             let imageee = magazinSubModel[indexPath.row].image ?? ""
             Companiescell.companyImage.contentMode = .scaleAspectFit
             Companiescell.configureCellamagazan(image: imageee)
-            
-        
-            
-            
-        Companiescell.selectionStyle = .none
+            Companiescell.selectionStyle = .none
             return Companiescell
         }
         return UITableViewCell()
     }
-
+    
+    
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 316
     }
@@ -256,9 +247,7 @@ extension MagazineHomeVC:UITableViewDelegate,UITableViewDataSource{
             navigationController?.pushViewController(vc, animated: true)
         }
     }
-
-
-  
+    
 }
 
 
@@ -271,56 +260,52 @@ extension MagazineHomeVC:FilterSubData{
         hud.textLabel.text = "جاري التحميل"
         hud.show(in: self.view)
         let sec_id = UserDefaults.standard.string(forKey: "FILTER_SEC_ID")
-        let sub_id = UserDefaults.standard.string(forKey: "FILTER_SUB_ID")
+//        let sub_id = UserDefaults.standard.string(forKey: "FILTER_SUB_ID")
         let coun_id = UserDefaults.standard.string(forKey: "FILTER_COUN_ID")
         let city_id = UserDefaults.standard.string(forKey: "FILTER_CITY_ID")
         let sort_val = UserDefaults.standard.string(forKey: "FILTER_SORT_VAL")
-
-
+        
+        
         DispatchQueue.global(qos: .background).async {
             let api_token = UserDefaults.standard.string(forKey: "API_TOKEN")
             let headers = ["Authorization": "\(api_token ?? "")" ]
             let param = ["type": "farm",  "country_id" : "\(coun_id ?? "1")" , "city_id" : "\(city_id ?? "1")" , "sort" : "\(sort_val ?? "0")"]
-
-            let SearchGuide = "https://elkenany.com/api/magazine/magazines?type=&sort=&city_id="
+            
+            let SearchGuide = "https://elkenany.com/api/magazine/magazines?type=&sort=&city_id=&country_id="
             APIServiceForQueryParameter.shared.fetchData(url: SearchGuide, parameters: param, headers: headers, method: .get) { (success:MagazineS?, filier:MagazineS?, error) in
                 if let error = error{
                     hud.dismiss()
                     print("============ error \(error)")
-                }else {
+                }else{
                     hud.dismiss()
                     self.magazinSubModel.removeAll()
                     let successData = success?.data?.data ?? []
                     self.magazinSubModel.append(contentsOf: successData)
+                    
                     DispatchQueue.main.async {
-
-                        if success?.data?.data?.isEmpty == true {
-                            print("hhhhhhhhhhhhhhhhhhhhhhhhhh")
-                        }
-
-
-
+//                        if success?.data?.data?.isEmpty == true {
+//                            print("hhhhhhhhhhhhhhhhhhhhhhhhhh")
+//                        }
                         self.magazinTV.reloadData()
                     }
                 }
             }
         }
     }
-
 }
 
 
 //MARK:- TableView for companies  [Methods + Delegets]
 
 extension MagazineHomeVC:UICollectionViewDelegate , UICollectionViewDataSource , UICollectionViewDelegateFlowLayout {
-
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return sectorSubModel.count
     }
-
-
+    
+    
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-
+        
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "SelectedSectorCell", for: indexPath) as! SelectedSectorCell
         cell.titleLabel.text = sectorSubModel[indexPath.item].name ?? ""
         
@@ -334,15 +319,15 @@ extension MagazineHomeVC:UICollectionViewDelegate , UICollectionViewDataSource ,
             cell.cooo.backgroundColor = #colorLiteral(red: 0.8039215686, green: 0.8039215686, blue: 0.8039215686, alpha: 1)
         }
         return cell
-
-         }
-
-
+        
+    }
+    
+    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize(width: 90, height: 60)
-
+        
     }
-
+    
 }
 
 
