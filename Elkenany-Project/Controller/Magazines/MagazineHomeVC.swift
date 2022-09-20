728faef878df1b62fb760f4c7dc7fb23f0b13c89
AbdelazizReda <48037088+AbdelazizReda@users.noreply.github.com>
2022-09-20 12:00:52 +0200

    last work at home magazine

--- Elkenany-Project/Controller/Magazines/MagazineHomeVC.swift
+++ Elkenany-Project/Controller/Magazines/MagazineHomeVC.swift
@@ -12,6 +12,8 @@ class MagazineHomeVC: UIViewController {
     
     //MARK:Outlets
     @IBOutlet weak var sectorsCV: UICollectionView!
+    @IBOutlet weak var magazineCollection: UICollectionView!
+    
     @IBOutlet weak var searchView: UIView!
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var view1: UIView!
@@ -22,6 +24,7 @@ class MagazineHomeVC: UIViewController {
     @IBOutlet weak var sortTtle: UIButton!
     @IBOutlet weak var cityTitle: UIButton!
     @IBOutlet weak var countryTitle: UIButton!
+    
     //vars
     var magazineHomeModel:MagazineS?
     var sectorSubModelMagazine:[Sectorrs] = []
@@ -46,17 +49,18 @@ class MagazineHomeVC: UIViewController {
     
     //MARK:- SetupUI + collectionView Delegete + tableView Delegete
     func setupUI() {
-        magazinTV.dataSource = self
-        magazinTV.delegate = self
         sectorsCV.delegate = self
         sectorsCV.dataSource = self
+        magazineCollection.delegate = self
+        magazineCollection.dataSource = self
         
         self.sectorsCV.register(UINib(nibName: "SelectedSectorCell", bundle: nil), forCellWithReuseIdentifier: "SelectedSectorCell")
-        magazinTV.register(UINib(nibName: "MagazineCell", bundle: nil), forCellReuseIdentifier: "MagazineCell")
-        magazinTV.prefetchDataSource = self
+        self.magazineCollection.register(UINib(nibName: "MagazineCell", bundle: nil), forCellWithReuseIdentifier: "MagazineCell")
+
+        magazineCollection.prefetchDataSource = self
         //Dynamice Hight cell
-        magazinTV.estimatedRowHeight = 150
-        magazinTV.rowHeight = UITableView.automaticDimension
+//        magazinTV.estimatedRowHeight = 150
+//        magazinTV.rowHeight = UITableView.automaticDimension
     }
     
     
@@ -105,7 +109,7 @@ class MagazineHomeVC: UIViewController {
                     print("current", self.currentpaga)
                     self.magazinSubModel.append(contentsOf: successDataa)
                     DispatchQueue.main.async {
-                        self.magazinTV.reloadData()
+                        self.magazineCollection.reloadData()
                     }
                     self.currentpaga += 1
                     self.isFeatchingData = false
@@ -141,7 +145,7 @@ class MagazineHomeVC: UIViewController {
                     self.magazinSubModel.append(contentsOf: successDataa)
                     
                     DispatchQueue.main.async {
-                        self.magazinTV.reloadData()
+                        self.magazineCollection.reloadData()
                     }
                     self.currentpaga += 1
                     self.isFeatchingData = false
@@ -171,7 +175,7 @@ class MagazineHomeVC: UIViewController {
                     let successData = success?.data?.data ?? []
                     self.magazinSubModel.append(contentsOf: successData)
                     DispatchQueue.main.async {
-                        self.magazinTV.reloadData()
+                        self.magazineCollection.reloadData()
                     }
                 }
             }
@@ -214,7 +218,7 @@ extension MagazineHomeVC : UISearchBarDelegate {
             SearchService()
         }
         //reload
-        magazinTV.reloadData()
+        magazineCollection.reloadData()
     }
     
     
@@ -243,8 +247,8 @@ extension MagazineHomeVC : UISearchBarDelegate {
 
 
 //MARK:pagination extension
-extension MagazineHomeVC:UITableViewDataSourcePrefetching {
-    func tableView(_ tableView: UITableView, prefetchRowsAt indexPaths: [IndexPath]) {
+extension MagazineHomeVC:UICollectionViewDataSourcePrefetching {
+    func collectionView(_ collectionView: UICollectionView, prefetchItemsAt indexPaths: [IndexPath]) {
         for index in indexPaths {
             if index.row >= magazinSubModel.count - 1 && !isFeatchingData {
                 FatchDatafromHome()
@@ -257,43 +261,7 @@ extension MagazineHomeVC:UITableViewDataSourcePrefetching {
 
 
 
-//MARK:- TableView for companies  [Methods + Delegets]
-extension MagazineHomeVC:UITableViewDelegate,UITableViewDataSource{
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return magazinSubModel.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if let Companiescell = tableView.dequeueReusableCell(withIdentifier: "MagazineCell") as? MagazineCell{
-//            Companiescell.companyName.text = magazinSubModel[indexPath.row].name ?? ""
-//            Companiescell.companyDesc.text = magazinSubModel[indexPath.row].desc ?? ""
-//            Companiescell.companyLocation.text = magazinSubModel[indexPath.row].address ?? ""
-//            Companiescell.rating.rating = magazinSubModel[indexPath.row].rate ?? 0.0
-//            let imageee = magazinSubModel[indexPath.row].image ?? ""
-//            Companiescell.companyImage.contentMode = .scaleAspectFit
-//            Companiescell.configureCellamagazan(image: imageee)
-//            Companiescell.selectionStyle = .none
-            return Companiescell
-        }
-        return UITableViewCell()
-    }
-    
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 160
-    }
-    
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        if let vc = storyboard?.instantiateViewController(withIdentifier: "MagazinVC") as? MagazinVC {
-            let id_magazin = magazinSubModel[indexPath.row].id ?? 0
-            vc.IdFromMagazine = id_magazin
-            navigationController?.pushViewController(vc, animated: true)
-        }
-    }
-    
-}
+
 
 
 
@@ -306,52 +274,90 @@ extension MagazineHomeVC:UITableViewDelegate,UITableViewDataSource{
 extension MagazineHomeVC:UICollectionViewDelegate , UICollectionViewDataSource , UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return sectorSubModelMagazine.count
+        
+        if collectionView == sectorsCV {
+            return sectorSubModelMagazine.count
+        }else{
+            return magazinSubModel.count
+        }
     }
     
     
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "SelectedSectorCell", for: indexPath) as! SelectedSectorCell
-        cell.titleLabel.text = sectorSubModelMagazine[indexPath.item].name ?? ""
-        if typeOfSectore == "poultry" {
-            cell.cooo.backgroundColor = #colorLiteral(red: 1, green: 0.5882352941, blue: 0, alpha: 1)
-            sectorsCV.selectItem(at: indexPath, animated: true, scrollPosition: .left)
+        
+        if collectionView == sectorsCV {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "SelectedSectorCell", for: indexPath) as! SelectedSectorCell
+            cell.titleLabel.text = sectorSubModelMagazine[indexPath.item].name ?? ""
+            if typeOfSectore == "poultry" {
+                cell.cooo.backgroundColor = #colorLiteral(red: 1, green: 0.5882352941, blue: 0, alpha: 1)
+                sectorsCV.selectItem(at: indexPath, animated: true, scrollPosition: .left)
+            }else{
+                cell.cooo.backgroundColor = #colorLiteral(red: 0.8039215686, green: 0.8039215686, blue: 0.8039215686, alpha: 1)
+            }
+            return cell
+            
         }else{
-            cell.cooo.backgroundColor = #colorLiteral(red: 0.8039215686, green: 0.8039215686, blue: 0.8039215686, alpha: 1)
+            let magazineecell = collectionView.dequeueReusableCell(withReuseIdentifier: "MagazineCell", for: indexPath) as! MagazineCell
+                //            Companiescell.companyName.text = magazinSubModel[indexPath.row].name ?? ""
+                //            Companiescell.companyDesc.text = magazinSubModel[indexPath.row].desc ?? ""
+                //            Companiescell.companyLocation.text = magazinSubModel[indexPath.row].address ?? ""
+                //            Companiescell.rating.rating = magazinSubModel[indexPath.row].rate ?? 0.0
+                //            let imageee = magazinSubModel[indexPath.row].image ?? ""
+                //            Companiescell.companyImage.contentMode = .scaleAspectFit
+                //            Companiescell.configureCellamagazan(image: imageee)
+                //            Companiescell.selectionStyle = .none
+            
+            return magazineecell
         }
-        return cell
+        
     }
     
     
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         
-        let sizeOne = (collectionView.frame.size.width - 35 ) / 5
-
-        return CGSize(width:sizeOne, height: 55)
+        if collectionView == sectorsCV {
+            let sizeOne = (collectionView.frame.size.width - 35 ) / 5
+            return CGSize(width:sizeOne, height: 55)
+            
+        }else{
+            let sizeTwo = (collectionView.frame.size.width - 20)
+            return CGSize(width:sizeTwo, height: 160)
+            
+        }
     }
     
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        print("clickkkkkkkkkk")
-        let typeOfSector = sectorSubModelMagazine[indexPath.item].type ?? "farm"
-        print("type ::: " , typeOfSector)
-        UserDefaults.standard.set(typeOfSector, forKey: "TYPE_FOR_FILTER")
-        self.typeOfSectore = typeOfSector
         
-        let cell = collectionView.cellForItem(at: indexPath) as! SelectedSectorCell
-        if(cell.isSelected == true)
-        {
-            cell.cooo.backgroundColor = #colorLiteral(red: 1, green: 0.5882352941, blue: 0, alpha: 1)
-            sectorsCV.selectItem(at: indexPath, animated: true, scrollPosition: .right)
+        if collectionView == sectorsCV {
+            let typeOfSector = sectorSubModelMagazine[indexPath.item].type ?? "farm"
+            print("type ::: " , typeOfSector)
+            UserDefaults.standard.set(typeOfSector, forKey: "TYPE_FOR_FILTER")
+            self.typeOfSectore = typeOfSector
+            
+            let cell = collectionView.cellForItem(at: indexPath) as! SelectedSectorCell
+            if(cell.isSelected == true)
+            {
+                cell.cooo.backgroundColor = #colorLiteral(red: 1, green: 0.5882352941, blue: 0, alpha: 1)
+                sectorsCV.selectItem(at: indexPath, animated: true, scrollPosition: .right)
+            }
+            FatchDatafromHomeHeader()
         }
-        FatchDatafromHomeHeader()
+        
+        else{
+            
+            if let vc = storyboard?.instantiateViewController(withIdentifier: "MagazinVC") as? MagazinVC {
+                let id_magazin = magazinSubModel[indexPath.row].id ?? 0
+                vc.IdFromMagazine = id_magazin
+                navigationController?.pushViewController(vc, animated: true)
+            }
+            
+        }
+        
     }
-    
-    
 }
-
 
 extension MagazineHomeVC: FilterShowMagazine {
     
@@ -366,8 +372,8 @@ extension MagazineHomeVC: FilterShowMagazine {
         let city_id = UserDefaults.standard.string(forKey: "FILTER_CITY_ID")
         let coun_title = UserDefaults.standard.string(forKey: "COUN_TITLE")
         let sort_title = UserDefaults.standard.string(forKey: "SORT_TITLE")
-
-
+        
+        
         let sort_val = UserDefaults.standard.string(forKey: "FILTER_SORT_VAL")
         
         let param = ["type": "\(sec_type ?? "horses")" ,  "country_id" : "\(coun_id ?? "1")" , "sort" : "\(sort_val ?? "1")"]
@@ -389,7 +395,7 @@ extension MagazineHomeVC: FilterShowMagazine {
                             print("hhhhhhhhhhhhhhhhhhhhhhhhhh")
                         }
                         
-                        self.magazinTV.reloadData()
+                        self.magazineCollection.reloadData()
                         print(success?.data ?? "")
                         self.countryTitle.setTitle( coun_title, for: .normal)
                         self.sortTtle.setTitle(sort_title, for: .normal)
